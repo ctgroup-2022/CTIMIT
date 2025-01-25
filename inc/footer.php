137accd7df30faf4ff378fd67b11e4e6e9613864
Arun Kumar <stockerunknown@gmail.com>
2025-01-25 13:16:07 +0530

    Adding More Files

---
+++ inc/footer.php
@@ -0,0 +1,181 @@
+
+
+  <!-- ======= Footer ======= -->
+
+  <footer id="footer">
+
+
+
+    <div class="footer-top">
+
+      <div class="container">
+
+        <div class="row">
+
+
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+
+            <h3>CTIMIT</h3>
+            <p>
+            Greater Kailash, Grand Trunk Road, <br>
+            Maqsudan, Jalandhar, <br>
+            Punjab 144008
+              <br><br>
+              <strong>Phone:</strong> +91-98146-46225,01815009605 <br>
+              <strong>Email:</strong> admissions.maqsudan@ctgroup.in<br>
+            </p>
+
+          </div>
+
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+
+             <h4>Useful Links</h4>
+
+            <ul>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="infra.php">Infrastructure</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="naac.php">NAAC</a></li>
+
+              <!-- <li><i class="bx bx-chevron-right"></i> <a href="https://ctimit.com/pdf/RTI%20letter%20Maqsudan.pdf" target="_blank">RTI</a></li> -->
+
+            
+
+            </ul>
+
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links"> 
+          <h4>Important Links</h4>
+          <ul>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="faculty.php">Faculty</a></li>
+
+           
+
+              <li><i class="bx bx-chevron-right"></i> <a href="https://ctimit.com/pdf/RTI%20LETTER040.pdf" target="_blank">RTI</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="arc.php">Anti-Ragging Committee</a></li>
+
+           
+
+<li><i class="bx bx-chevron-right"></i> <a href="scobc.php"> SC/ST/OBC Committee</a></li>
+
+            
+
+            </ul>
+
+          </div>
+
+          <div class="col-lg-4 col-md-3 col-sm-3">
+
+          <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d109098.93130479497!2d75.51141963054336!3d31.294302293114498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d31.2285854!2d75.5625925!4m5!1s0x391a50711d4986d3%3A0xed348b061524c132!2sCT%20Group%20Of%20Institutions%2C%20Greater%20Kailash%2C%20Grand%20Trunk%20Road%2C%20Maqsudan%2C%20Jalandhar%2C%20Punjab%20144008!3m2!1d31.357934399999998!2d75.560256!5e0!3m2!1sen!2sin!4v1692959597333!5m2!1sen!2sin" width="350" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+            <iframe src="https://maps.google.com/maps?q=%209H56+26P,%20Greater%20Kailash,%20Moti%20Nagar,%20Jalandhar,%20Punjab%20144027&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 300px; height: 250px;"></iframe>
+            </div>
+<!--    
+
+          <div class="col-lg-3 col-md-6 footer-links">
+
+            <h4>Our Services</h4>
+
+            <ul>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+
+            </ul>
+
+          </div> -->
+
+<!-- 
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+
+            <h4>Join Our Newsletter</h4>
+
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+
+            <form action="" method="post">
+
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+
+            </form>
+
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+    </div> -->
+
+
+
+    <div class="container d-md-flex py-4">
+
+
+
+      <div class="me-md-auto text-center text-md-start">
+
+        <div class="copyright">
+
+          &copy;  <strong>Developed & Maintained By Aryana Software Development Cell</strong>
+
+        </div>
+
+        <div class="credits">
+
+          <!-- All the links in the footer should remain intact. -->
+
+          <!-- You can delete the links only if you purchased the pro version. -->
+
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
+
+          <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
+
+        </div>
+
+      </div>
+
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+
+        <a href="https://www.facebook.com/ct.institutions" class="facebook"><i class="bx bxl-facebook"></i></a>
+
+        <a href="https://www.instagram.com/ctnorthcampus/" class="instagram"><i class="bx bxl-instagram"></i></a>
+
+        <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
+
+      </div>
+
+      <div class="">
+
+     
+
+        
+
+      </div>
+
+    </div>
+
+  </footer><!-- End Footer -->
---
+++ inc/header.php
@@ -0,0 +1,123 @@
+ <!-- ======= Header ======= -->
+  <header id="header" class="position-relative">
+    <div class="container d-flex align-items-center" style="padding-top: 10px;">
+      <a href="index.php"> <img src="assets/img/CTIMIT_LOGO.png" style="height:70px; width:%;"></a>
+      <!-- <h1 class="logo me-auto"><a href="index.html">CTIMIT</a></h1> -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a class="" href="bog.php">Governing Body</a></li>
+          <li class="dropdown"><a  href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="vision&mission.php">Vision & Mission</a></li>
+              <li><a href="management.php">From Management's Desk</a></li>
+              <li><a href="about.php">About Campus</a></li>
+             
+            </ul>
+          </li>
+          <!-- <li><a href="courses.html">Programs</a></li> -->
+          <li class="dropdown"><a href="#"><span>Programs</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+             <li class="dropdown"><a href="#"><span>UG Programs</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="jmc.php">Bachelor of Arts (Journalism and Mass Communication)</a></li>
+                  <li><a href="bsc.php">Bachelor of Science (Mutlimedia)</a></li>
+                  <li><a href="bca.php">Bachelor in Computer Applications</a></li>
+                  <li><a href="hotel.php">Bachelor in Hotel Management & Catering Technology (AICTE)</a></li>
+                  <li><a href="bba.php">Bachelor of Business Administration</a></li>
+                  <li><a href="commerce.php">Bachelor of Commerce (Hons.)</a></li>
+                  <li><a href="hm&ct.php">Bachelor in Hotel Management & Catering Technology</a></li>
+                  <li><a href="bttm.php">Bachelor of Tourism and Travel Management</a></li>
+                  <!-- <li><a href="bvoc.php">Bachelor of Vocation (Beauty Therapy and Asethetics)</a></li> -->
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>PG Programs</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="mba.php">MBA</a></li>
+                  <li><a href="mca.php">Master in Computer Applications</a></li>
+                </ul>
+              </li>
+             
+            </ul>
+          </li>
+          <li class="dropdown"><a  href="#"><span>Why CTIMIT?</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="alumni.php">Alumni</a></li>
+              <li><a href="placement.php">Placement</a></li>
+              <!-- <li><a href="about.php">Tie-Ups</a></li> -->
+             
+            </ul>
+          </li><li class="dropdown"><a  href="#"><span>Departments</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="managmentstudies-dept.php">Department of Management Studies</a></li>
+              <li><a href="mediastudies-dept.php">Department of Media Studies</a></li>
+              <li><a href="commerce-dept.php">Department of Commerce</a></li>
+              <li><a href="hotelmanagement-dept.php">Department of Hotel Management</a></li>
+              <li><a href="computerapplication-dept.php">Department of Computer Application</a></li>
+              <li><a href="tourism&travel-dept.php">Department of Tourism and Travel Management</a></li>
+            </ul>
+          </li>
+          <!-- <li><a href="ctimit.php">Why CTIMIT</a></li> -->
+          <li><a href="infra.php">Infrastructure</a></li>
+          <li class="dropdown"><a href="naac.php"><span>NAAC</span><i class="bi bi-chevron-down"></i></a>
+          <ul>
+              <li><a href="aqar.php">AQAR</a></li>
+</ul>
+</li>
+             
+
+
+          <li class="dropdown"><a href="#"><span>Cell</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href="iqac.php"><span>IQAC</span> 
+              <!-- <i class="bi bi-chevron-right"></i> -->
+            </a>
+                <ul>
+                  <!-- <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li> -->
+                </ul>
+              </li>
+              <!-- <li><a href="examcell.php">Examination Cell</a></li> -->
+              <li><a href="icc.php">Internal Compliant Committee</a></li>
+              <!-- <li><a href="wec.php">Women Empowerment Cell</a></li> -->
+              <li><a href="arc.php">Anti-Ragging Committee</a></li>
+              
+              <!-- <li><a href="ac.php">Admission Cell</a></li> -->
+              <!-- <li><a href="ccpc.php">CCPC</a></li> -->
+              <!-- <li><a href="hr.php">Human Resource Department</a></li> -->
+              <li><a href="sgc.php" target="_blank">Grievance Redressal Committee</a></li>
+           <li><a href="scobc.php"> SC/ST/OBC Committee </a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="nss.php">NSS</a></li>
+              <li><a href="https://ctimit.com/pdf/RTI%20LETTER040.pdf">RTI</a></li>
+              <li><a href="https://ctimit.com/faculty.php">Faculty</a></li>
+              <li><a href="feedback.php">Feedback</a></li>
+              <li><a href="affiliations.php">Affiliations</a></li>
+              <li><a href="https://ctimit.com/pdf/Strategy-plan.pdf" target="_blank">Strategy Plan</a></li>
+              <li><a href="https://ctimit.com/pdf/HR POLICY Final.pdf" target="_blank">HR Policy</a></li>
+              <li class="dropdown"><a href="#"><span>E-Books</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="https://ctimit.com/pdf/e-books/ICTESM-2020.pdf" target="_blank">ICTESM-2020</a></li>
+                  <li><a href="https://ctimit.com/pdf/e-books/ICTESM-2019.pdf" target="_blank">ICTESM-2019</a></li>
+                </ul>
+              </li>
+              <li><a href="https://cthalfmarathon.in/index.php">Half Marathon 2024</a></li>
+            </ul>
+          </li>
+          <li><a href="http://naac.gov.in/index.php/en/19-quick-links/133-screen-reader">Screen Reader</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+  
+    </div>
+  </header><!-- End Header -->
